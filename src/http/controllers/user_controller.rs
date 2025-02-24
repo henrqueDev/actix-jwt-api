aebc268685e6c35e6f347aec311c4af59230845d
henrqueDev <lhdeveloper137@gmail.com>
2025-02-24 01:08:51 -0300

    :zap: perf: improving bcrypt hash speed in 4x for better time response (cost 12 to 10)

--- src/http/controllers/user_controller.rs
+++ src/http/controllers/user_controller.rs
@@ -1,11 +1,13 @@
-use actix_web::{web::{self, ServiceConfig}, HttpResponse, Responder};
-use chrono::Local;
-use crate::{http::requests::user::user_store_request::UserStoreRequest, model::user::UserDTO, services::auth::encode_jwt};
+use actix_web::{http::header::ContentType, web::{self, ServiceConfig}, HttpResponse, Responder};
+use chrono::Utc;
+use diesel::{ ExpressionMethods, QueryDsl, SelectableHelper};
+use diesel_async::RunQueryDsl;
+use crate::{database::db::get_connection, http::{requests::user::user_store_request::UserStoreRequest, responses::user::user_store_response::{UserStoreError, UserStoreResponse}}, model::user::{user::User, user_dto::UserDTO}, schema::users};
 
 pub async fn store(body: web::Json<UserStoreRequest>) -> impl Responder {
     let mut data = body.into_inner();
     
-    data.password = match bcrypt::hash(&data.password, 12) {
+    data.password = match bcrypt::hash(&data.password, 10) {
         Ok(password) => password,
         Err(_err) => panic!("Error while bcrypt password")
     };
