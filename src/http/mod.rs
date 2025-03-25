8ce43d0c3366edaab86b16910606c9b2e00c15ff
henrqueDev <lhdeveloper137@gmail.com>
2025-03-24 22:42:12 -0300

    :label: raw: generic responses/errors structs

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,4 +1,17 @@
+use serde::Serialize;
+
 pub mod controllers;
 pub mod requests;
 pub mod responses;
 pub mod middleware;
+
+#[derive(Serialize)]
+pub struct GenericResponse<'a> {
+    pub message: &'a str
+}
+
+#[derive(Serialize)]
+pub struct GenericError<'a> {
+    pub message: &'a str,
+    pub error: &'a str
+}
