1e12bf4450440c4043a83d1379f64ecc0b570c85
henrqueDev <lhdeveloper137@gmail.com>
2025-03-24 22:46:27 -0300

    :label: raw: code field for body login struct

--- src/http/requests/auth/auth_login_request.rs
+++ src/http/requests/auth/auth_login_request.rs
@@ -3,5 +3,6 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct AuthLoginRequest {
     pub email: String,
-    pub password: String
+    pub password: String,
+    pub code: Option<String>
 }
