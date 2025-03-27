eb4c193a8544ae6dfe5f2f4df242afd82ecc513a
henrqueDev <lhdeveloper137@gmail.com>
2025-03-27 00:47:02 -0300

    :bulb: docs: remember to solve genericError issue

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -18,6 +18,7 @@ pub struct GenericError<'a> {
     pub error: &'a str
 }
 
+// Retornando string na resposta ao invés de JSON (Revisar)
 impl Display for GenericError<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{{ \"message\":{:#?}, \"error\":{:#?} }}", self.message, self.error)
