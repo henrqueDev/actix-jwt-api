fbfb0c68681c1f34950423079c57b5d2834c9ab9
henrqueDev <lhdeveloper137@gmail.com>
2025-04-07 03:33:02 -0300

    :lock: raw: get client ip secure method

--- src/http/middleware/brute_force_wall_middleware.rs
+++ src/http/middleware/brute_force_wall_middleware.rs
@@ -1,21 +1,17 @@
-use std::net::{SocketAddr, ToSocketAddrs};
 
 use actix_web::{
     body::MessageBody,
     dev::{ServiceRequest, ServiceResponse},
-    middleware::{Logger, Next},
+    middleware::Next,
     Error,
 };
-use diesel::{ExpressionMethods, QueryDsl, SelectableHelper};
-use diesel_async::RunQueryDsl;
 
-use crate::{database::db::get_connection, http::GenericError, model::user::user::User, schema::users, services::auth::decode_jwt};
 
 pub async fn brute_force_wall_middleware(
     req: ServiceRequest,
     next: Next<impl MessageBody>,
 ) -> Result<ServiceResponse<impl MessageBody>, Error> {
-    let requisicao = req.peer_addr().unwrap().ip();
+    let requisicao = req.connection_info().peer_addr().unwrap().to_owned();
     println!("{:#?}", requisicao);
     next.call(req).await
 }
